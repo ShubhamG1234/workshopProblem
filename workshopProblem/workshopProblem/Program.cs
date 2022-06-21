c97b3561a2d35854868a942e74d09fcc51935bc7
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-06-21 11:02:18 +0530

    leap year problem

--- workshopProblem/workshopProblem/Program.cs
+++ workshopProblem/workshopProblem/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+//leap year problem
+namespace workshopProblem
+{
+    class LeapYear
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Year: ");
+            int checkingYear;
+
+            checkingYear = Convert.ToInt32(Console.ReadLine());
+
+
+            if ((checkingYear % 4) == 0)
+                Console.WriteLine("This is a Leap Year");
+
+            else
+            {
+                Console.WriteLine("This is not a Leap Year");
+            }
+
+
+
+        }
+    }
+}
